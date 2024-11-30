0020201422cccfc666c0554be435a1d6c10cf1bd
keiko233 <i@elaina.moe>
2024-12-01 02:03:22 +0800

    feat(tailwind): add utility for height subtraction using 100dvh

--- packages/tailwind/index.js
+++ packages/tailwind/index.js
@@ -53,6 +53,18 @@ module.exports = plugin(
     }, {});
 
     addUtilities(mdColors, ["responsive", "hover", "focus"]);
+
+    const heights = theme("height");
+
+    const subtractRules = Object.keys(heights).reduce((acc, key) => {
+      const originalValue = heights[key];
+      acc[`.${e(`h-dvh-subtract-${key}`)}`] = {
+        height: `calc(100dvh - ${originalValue})`,
+      };
+      return acc;
+    }, {});
+
+    addUtilities(subtractRules, { variants: ["responsive"] });
   },
   {
     theme: {
