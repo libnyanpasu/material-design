5fe311ea9d3cc0ac472c71f31c2517808314b828
keiko233 <i@elaina.moe>
2024-12-03 21:38:48 +0800

    fix(storybook): tailwind plugin import

---
+++ apps/storybook/tailwind.config.ts
@@ -0,0 +1,11 @@
+import mdplugin from "@nyanpasu/material-design-tailwind";
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "../../packages/react/src/**/*.{ts,tsx}",
+    "../../packages/components/src/**/*.{ts,tsx}",
+  ],
+  plugins: [mdplugin],
+} satisfies Config;
