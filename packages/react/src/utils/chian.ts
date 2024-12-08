8f28d218310fd6440848acf4fd9c48c70a0dce7e
keiko233 <i@elaina.moe>
2024-12-09 00:56:33 +0800

    fix(chains): type definition

--- packages/react/src/utils/chian.ts
+++ packages/react/src/utils/chian.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function chains<T extends React.SyntheticEvent>(
+export function chains<T>(
   ...handlers: Array<((event: T) => void) | undefined>
 ) {
   return (event: T) => {
