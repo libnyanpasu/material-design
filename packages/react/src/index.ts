c4a215b407cb366a57b1c666caecfacc7eae78be
keiko233 <i@elaina.moe>
2025-01-07 22:38:04 +0800

    fix(index): export dropdown menu component from index file

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./card";
 export * from "./color";
+export * from "./dropdown-menu";
 export * from "./input";
 export * from "./provider";
 export * from "./select";
