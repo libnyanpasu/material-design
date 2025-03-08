868ea449145360c50a332593a3845627751e7af6
keiko233 <i@elaina.moe>
2025-03-08 21:48:40 +0800

    fix(input): enhance dark mode support for input label focus styles

--- packages/components/src/input/index.ts
+++ packages/components/src/input/index.ts
@@ -33,7 +33,8 @@ export type InputVariants = VariantProps<typeof inputVariants>;
 export const inputLabelVariants = tcva(
   [
     selectLabelVariants.base,
-    "peer-focus:top-2 peer-focus:text-xs peer-focus:text-primary",
+    "peer-focus:top-2 peer-focus:text-xs",
+    "peer-focus:text-primary dark:peer-focus:text-surface",
   ],
   {
     variants: {
